3a1baa30d658557a06ac841d3a9e75b7faae0482
Ana Meisel <aanameisell@gmail.com>
2019-10-06 23:50:41 +0100

    added images and videos

--- js/maz.js
+++ js/maz.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
 	$('textarea').each(function() {
 		$(this).height($(this).prop('scrollHeight'));
 	});
@@ -37,4 +36,35 @@ $(document).ready(function() {
 		$(".six").toggle();
 	});
 
+	$(".seven").hide();
+	$("#nextButton7").click(function() {
+		$(".seven").toggle();
+	});
+
+	$(".eight").hide();
+	$("#nextButton8").click(function() {
+		$(".eight").toggle();
+	});
+
 });
+
+// window.onload = function() {
+// 	// Video
+// 	var video = document.getElementById("video");
+// 	// Buttons
+// 	var playButton = document.getElementById("play-pause");
+
+// 	playButton.addEventListener("click", function() {
+// 	  	if (video.paused == true) {
+// 		    // Play the video
+// 		    video.play();
+// 		    // Update the button text to 'Pause'
+// 		    playButton.innerHTML = "Pause";
+// 		  } else {
+// 		    // Pause the video
+// 		    video.pause();
+// 	    // Update the button text to 'Play'
+// 	    playButton.innerHTML = "Play";
+// 	  	}
+// 	});
+// }
